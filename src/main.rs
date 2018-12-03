c443d67e6fee513d0fcc048e6f6b6e6a167bc40b
Robert Wallis <smilingrob@gmail.com>
2018-12-03 10:46:39 -0800

    refactored into logical functions, put in loop

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,29 @@ fn main() {
     let min = 1;
     let max = 100;
 
-    let mut random = rand::thread_rng();
-    let answer = random.gen_range(min, max + 1);
+    let answer = gen_answer(min, max);
 
+    loop {
+        let guess = ask_for_guess(min, max);
+        match check_answer(answer, guess) {
+            Result::Ok(_) => {
+                println!("You Win!");
+                break;
+            },
+            Result::Err(err) => println!("Nope: {}", err),
+        }
+    }
+
+    println!("The answer was: {}", answer);
+}
+
+/// Generate a number between min and max inclusive [min, max].
+fn gen_answer(min: i32, max: i32) -> i32 {
+    let mut random = rand::thread_rng();
+    return random.gen_range(min, max + 1);
+}
+
+fn ask_for_guess(min: i32, max: i32) -> i32 {
     print!("Guess a number between {}, and {}: ", min, max);
     io::stdout().flush().expect("stdout can't flush");
 
@@ -23,12 +43,13 @@ fn main() {
         .expect("Read Line Failed :/");
 
     let guess: i32 = guess.trim().parse().expect("Not a number");
+    return guess;
+}
 
-    match guess.cmp(&answer) {
-        Ordering::Less => println!("Too Low"),
-        Ordering::Greater => println!("Too High"),
-        Ordering::Equal => println!("Yes"),
-    }
-
-    println!("The answer was: {}", answer);
+fn check_answer(answer: i32, guess: i32) -> Result<(), String> {
+    return match guess.cmp(&answer) {
+        Ordering::Less => Result::Err("Too low.".to_string()),
+        Ordering::Greater => Result::Err("Too high.".to_string()),
+        Ordering::Equal => Result::Ok(()),
+    };
 }
