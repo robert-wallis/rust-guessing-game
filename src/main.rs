dea07461736e5e51159836f093a19ba4202d4c6a
Robert Wallis <smilingrob@gmail.com>
2018-12-03 10:28:21 -0800

    Generates random, reads input, compares, exits.  No loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing Game");
+
+    let min = 1;
+    let max = 100;
+
+    let mut random = rand::thread_rng();
+    let answer = random.gen_range(min, max + 1);
+
+    print!("Guess a number between {}, and {}: ", min, max);
+    io::stdout().flush().expect("stdout can't flush");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Read Line Failed :/");
+
+    let guess: i32 = guess.trim().parse().expect("Not a number");
+
+    match guess.cmp(&answer) {
+        Ordering::Less => println!("Too Low"),
+        Ordering::Greater => println!("Too High"),
+        Ordering::Equal => println!("Yes"),
+    }
+
+    println!("The answer was: {}", answer);
 }
