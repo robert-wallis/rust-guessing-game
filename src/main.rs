c0d2e03791b3928e754b354e542f3526f13a388a
Robert Wallis <smilingrob@gmail.com>
2018-12-05 10:30:55 -0800

    Now using Elm like Message Loop

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,89 @@ struct Stats {
     turns: i32,
 }
 
-struct GameState {
+struct Range {
     min: i32,
     max: i32,
-    answer: i32,
-    stats: Stats,
+}
+impl Range {
+    /// Generate a number between min and max inclusive [min, max].
+    fn random(&self) -> i32 {
+        let mut random = rand::thread_rng();
+        random.gen_range(self.min, self.max + 1)
+    }
 }
 
 enum AskGuessError {
     IOError(io::Error),
     NotANumber,
+}
+
+trait Guesser {
+    fn guess(&self, range: &Range) -> Result<i32, AskGuessError>;
+}
+trait Displayer {
+    fn display_guess_error(&self, err: &AskGuessError);
+    fn display_guess_result(&self, result: &GuessResult);
+    fn display_stats(&self, stats: &Stats);
+}
+
+struct StdIoGuesser;
+
+impl Guesser for StdIoGuesser {
+    fn guess(&self, range: &Range) -> Result<i32, AskGuessError> {
+        print!("Guess a number between {}, and {}: ", range.min, range.max);
+        io::stdout().flush()?;
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)?;
+
+        match guess.trim().parse() {
+            Ok(num) => Ok(num),
+            Err(_) => Err(AskGuessError::NotANumber),
+        }
+    }
+}
+
+impl Displayer for StdIoGuesser {
+    fn display_guess_error(&self, err: &AskGuessError) {
+        println!("{}", err)
+    }
+    fn display_guess_result(&self, result: &GuessResult) {
+        println!("{}", result)
+    }
+    fn display_stats(&self, stats: &Stats) {
+        println!("You figured it out in {} tries.", stats.turns)
+    }
+}
+
+enum GuessResult {
+    TooHigh(i32),
+    TooLow(i32),
+    Correct(i32),
+}
+
+enum Message {
+    GenerateAnswer(Range),
+    AskForGuess {
+        answer: i32,
+        range: Range,
+        stats: Stats,
+    },
+    CheckGuess {
+        guess: i32,
+        answer: i32,
+        range: Range,
+        stats: Stats,
+    },
+    ShowGuessResult {
+        result: GuessResult,
+        answer: i32,
+        range: Range,
+        stats: Stats,
+    },
+    ShowStats {
+        stats: Stats,
+    },
 }
 
 impl From<io::Error> for AskGuessError {
@@ -41,63 +114,111 @@ impl fmt::Display for AskGuessError {
     }
 }
 
-fn main() {
-    println!("Guessing Game");
-
-    let mut game_state = GameState {
-        min: 1,
-        max: 100,
-        answer: 0,
-        stats: Stats { turns: 0 },
-    };
-
-    game_state.answer = gen_answer(game_state.min, game_state.max);
-
-    loop {
-        game_state.stats.turns += 1;
-        let guess = ask_for_guess(game_state.min, game_state.max);
-        match guess {
-            Ok(guess) => {
-                match guess.cmp(&game_state.answer) {
-                    Ordering::Less => {
-                        println!("Too low.");
-                        game_state.min = guess + 1;
-                    }
-                    Ordering::Greater => {
-                        println!("Too high.");
-                        game_state.max = guess - 1;
-                    }
-                    Ordering::Equal => {
-                        println!("You Win!");
-                        break;
-                    }
-                };
-            }
-            Err(err) => println!("{}", err),
+impl fmt::Display for GuessResult {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            GuessResult::TooLow(guess) => write!(f, "{} is too low.", guess),
+            GuessResult::TooHigh(guess) => write!(f, "{} is too high.", guess),
+            GuessResult::Correct(answer) => write!(f, "{} is correct.", answer),
         }
     }
-
-    println!(
-        "The answer was: {}, you got it in {} turns.",
-        game_state.answer, game_state.stats.turns
-    );
 }
 
-/// Generate a number between min and max inclusive [min, max].
-fn gen_answer(min: i32, max: i32) -> i32 {
-    let mut random = rand::thread_rng();
-    random.gen_range(min, max + 1)
-}
+fn main() {
+    println!("==== Guessing Game ====");
 
-fn ask_for_guess(min: i32, max: i32) -> Result<i32, AskGuessError> {
-    print!("Guess a number between {}, and {}: ", min, max);
-    io::stdout().flush()?;
+    let mut msg = Message::GenerateAnswer(Range { min: 1, max: 100 });
+    let stdio = StdIoGuesser;
+    let guesser: &Guesser = &stdio;
+    let displayer: &Displayer = &stdio;
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)?;
-
-    match guess.trim().parse() {
-        Ok(num) => Ok(num),
-        Err(_) => Err(AskGuessError::NotANumber),
+    loop {
+        msg = match msg {
+            Message::GenerateAnswer(range) => Message::AskForGuess {
+                answer: range.random(),
+                range,
+                stats: Stats { turns: 0 },
+            },
+            Message::AskForGuess {
+                answer,
+                range,
+                stats,
+            } => match guesser.guess(&range) {
+                Ok(guess) => Message::CheckGuess {
+                    guess,
+                    answer,
+                    range,
+                    stats,
+                },
+                Err(err) => {
+                    displayer.display_guess_error(&err);
+                    Message::AskForGuess {
+                        answer,
+                        range,
+                        stats,
+                    }
+                }
+            },
+            Message::CheckGuess {
+                guess,
+                answer,
+                range,
+                stats,
+            } => {
+                let stats = Stats {
+                    turns: stats.turns + 1,
+                };
+                match guess.cmp(&answer) {
+                    Ordering::Less => Message::ShowGuessResult {
+                        result: GuessResult::TooLow(guess),
+                        answer,
+                        range: Range {
+                            min: guess + 1,
+                            ..range
+                        },
+                        stats,
+                    },
+                    Ordering::Greater => Message::ShowGuessResult {
+                        result: GuessResult::TooHigh(guess),
+                        answer,
+                        range: Range {
+                            max: guess - 1,
+                            ..range
+                        },
+                        stats,
+                    },
+                    Ordering::Equal => {
+                        let result = GuessResult::Correct(answer);
+                        Message::ShowGuessResult {
+                            result,
+                            answer,
+                            range,
+                            stats,
+                        }
+                    }
+                }
+            }
+            Message::ShowGuessResult {
+                result,
+                answer,
+                range,
+                stats,
+            } => {
+                displayer.display_guess_result(&result);
+                match result {
+                    GuessResult::Correct(_) => Message::ShowStats { stats },
+                    _ => Message::AskForGuess {
+                        answer,
+                        range,
+                        stats,
+                    },
+                }
+            }
+            Message::ShowStats { stats } => {
+                displayer.display_stats(&stats);
+                break;
+            }
+        }
     }
+    println!("End Game");
 }
